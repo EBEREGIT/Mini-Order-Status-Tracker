13d6052e0c20c5ec46fdf84fbd05a60cacd38b8e
eberegit <samsoneberenjoku@gmail.com>
2024-10-25 11:44:44 +0100

    updated env file with BASE_URL

--- src/providers/trpc.tsx
+++ src/providers/trpc.tsx
@@ -4,13 +4,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
 
-export default function TRPCProvider({ children }: { children: React.ReactNode }) {
+export default function TRPCProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${process.env.BASE_URL}api/trpc`,
         }),
       ],
     })
