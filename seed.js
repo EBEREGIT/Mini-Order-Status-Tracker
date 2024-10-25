80b6a0fc643bb5657963eb7e46ed3868d6ddd636
eberegit <samsoneberenjoku@gmail.com>
2024-10-25 11:27:08 +0100

    populated the database using seed.js file

--- seed.js
+++ seed.js
@@ -1,25 +1,26 @@
-import { PrismaClient } from '@prisma/client';
-import { faker } from '@faker-js/faker';
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // Create 25 users
+  // Create 10 users
   const users = await Promise.all(
-    Array.from({ length: 25 }).map(() =>
+    Array.from({ length: 10 }).map(() =>
       prisma.user.create({
         data: {
-          name: faker.name.fullName(),
-          address: faker.address.streetAddress() + ', ' + faker.address.city(),
+          name: faker.person.fullName(),
+          address:
+            faker.location.streetAddress() + ", " + faker.location.city(),
           email: faker.internet.email(),
         },
       })
     )
   );
 
-  // Create 50 products
+  // Create 10 products
   const products = await Promise.all(
-    Array.from({ length: 50 }).map(() =>
+    Array.from({ length: 10 }).map(() =>
       prisma.product.create({
         data: {
           name: faker.commerce.productName(),
@@ -30,24 +31,27 @@ async function main() {
     )
   );
 
-  // Generate 500 orders, each associated with a random user
-  for (let i = 0; i < 500; i++) {
+  // Generate 100 orders, each associated with a random user
+  for (let i = 0; i < 100; i++) {
     // Randomly select a user for this order
     const randomUser = users[Math.floor(Math.random() * users.length)];
     await prisma.order.create({
       data: {
         userId: randomUser.id,
         fulfillmentStatus: faker.helpers.arrayElement([
-          'Pending',
-          'Shipped',
-          'Delivered',
+          "Pending",
+          "Shipped",
+          "Delivered",
         ]),
         lineItems: {
-          create: Array.from({ length: faker.datatype.number({ min: 1, max: 5 }) }).map(() => {
-            const randomProduct = products[Math.floor(Math.random() * products.length)];
+          create: Array.from({
+            length: faker.number.int({ min: 1, max: 5 }),
+          }).map(() => {
+            const randomProduct =
+              products[Math.floor(Math.random() * products.length)];
             return {
               productId: randomProduct.id,
-              quantity: faker.datatype.number({ min: 1, max: 3 }),
+              quantity: faker.number.int({ min: 1, max: 3 }),
             };
           }),
         },
@@ -55,7 +59,7 @@ async function main() {
     });
   }
 
-  console.log('Database seeded with 25 users, 50 products, and 500 orders.');
+  console.log("Database seeded with 10 users, 10 products, and 100 orders.");
 }
 
 main()
